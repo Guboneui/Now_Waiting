c1aba50c7c1a1bdd60aaed929745383b363c9708
starku2249 <starku2249@naver.com>
2020-12-22 01:29:24 +0900

    클래스 공용 배열 선언

--- home.py
+++ home.py
@@ -4,4 +4,4 @@ class Home:
     guestList = []          # 당일 방문하는 고객들 담는 리스트 / clear 통해서 영업 종료시 초기화
     allList = []            # guestList 와 동일하게 저장되지만 초기화 되지 않음, 값 중복 가능. => Counter 통해서 고객 방문 횟수 확인 가능
     todayguest = []         # 쿠폰 중복 방지 용
-    copyGuestCoupon = []
+    copyGuestCoupon = []    # 계산을 위한 복사용 리스트
