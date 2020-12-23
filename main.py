7385101cdf5afae0ac930eed39d00784394f7fa6
starku2249 <starku2249@naver.com>
2020-12-23 12:57:20 +0900

    클래스 호출(최초 화면)

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+#클래스 호출
 import guest
 import store
-#클래스 호출
 
 k = input("매장 점주님이 신가요? Y/N")
 if k.lower() == "y":
     kkk = store.Store()
-    print(kkk.show1())
+    print(kkk.showStore())
 
 elif k.lower() == "n":
     kkk = guest.Guest()
-    print(kkk.show2())
+    print(kkk.showGuest())
+
